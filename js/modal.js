86aa1af587fdd80ea1f3cc988208035a805e7881
Ryan Boehm <ryan.w.boehm@gmail.com>
2022-04-06 17:46:34 -0400

    update section title and text

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,7 @@ jQuery(function($){
   $('.modal-wrap').on('click', '.close-icon',function() {
     $('.modal-wrap').toggleClass('closed');
     $('.modal-overlay').toggleClass('closed');
+    $('body').toggleClass('overflowHidden');
   });
 
   $('body').click(function (event) {
@@ -11,6 +12,7 @@ jQuery(function($){
       if(!$(event.target).closest('.modal-wrap').length && !$(event.target).is('.modal-wrap')) {
         $('.modal-wrap').addClass('closed');
         $('.modal-overlay').addClass('closed');
+        $('body').removeClass('overflowHidden');
       }
     }
   });
